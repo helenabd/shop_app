91c35042deb9472430cadd6371f5508eb6efdfec
Helena Brito <helena_b_dias@hotmail.com>
2020-12-18 12:12:47 -0300

    DELETE Request and Custom Exceptions

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_complete_guide/models/http_exception.dart';
 import 'package:http/http.dart' as http;
 
 import './product.dart';
@@ -143,8 +144,21 @@ class Products with ChangeNotifier {
     }
   }
 
-  void deleteProduct(String id) {
-    _items.removeWhere((product) => product.id == id);
+  Future<void> deleteProduct(String id) async {
+    final url = 'https://fir-8fa2d.firebaseio.com/products/$id.json';
+    final existingProductIndex =
+        _items.indexWhere((product) => product.id == id);
+    var existingProduct = _items[existingProductIndex];
+
+    _items.removeAt(existingProductIndex);
     notifyListeners();
+
+    final response = await http.delete(url);
+    if (response.statusCode >= 400) {
+      _items.insert(existingProductIndex, existingProduct);
+      notifyListeners();
+      throw HttpException('Could no delete product.');
+    }
+    existingProduct = null;
   }
 }
